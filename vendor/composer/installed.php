7b454a5eb5d40fc9f90b62f26039dea488a13571
dansbba <sakana_bun@yahoo.co.jp>
2021-03-31 03:37:28 +0900

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'deccd98e47ba37d23cf22e8741668673056334a1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'deccd98e47ba37d23cf22e8741668673056334a1',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '7.0.0',
+      'version' => '7.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '210d87af3f5fa3ed7837c3c875e39e1e3caeb9b7',
+    ),
+  ),
+);
